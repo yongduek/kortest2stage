8c36315b13627f36fc52300fdcab05a70a85c90e
서용덕 Yongduek Seo <yongduek.seo@gmail.com>
2024-07-30 10:58:29 +0900

    0

--- kortest/models.py
+++ kortest/models.py
@@ -4,24 +4,21 @@ from django.db import models
 
 class Item(models.Model):  # A Question Item
     qtext  = models.CharField(max_length=200)
-    qjimun = models.CharField(max_length=1000)
-    # qjimun_image = models.ImageField(upload_to='appname', null=True) # image 
+    jimun = models.CharField(max_length=1000)
+    # jimun_image = models.ImageField(upload_to='appname', null=True, blank=True) # image 
 
-    choice1 = models.CharField(max_length=200)
-    choice2 = models.CharField(max_length=200)
-    choice3 = models.CharField(max_length=200)
-    choice4 = models.CharField(max_length=200)
+    ch1 = models.CharField(max_length=200) # choice 1
+    ch2 = models.CharField(max_length=200)
+    ch3 = models.CharField(max_length=200)
+    ch4 = models.CharField(max_length=200)
 
     correct_answer = models.IntegerField()
 
     create_datetime = models.DateTimeField(auto_now=True, null=True)
 
 class TestSheet(models.Model):
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
-
-class ListofTestSheets(models.Model):
-    testsheet = models.ForeignKey(TestSheet, on_delete=models.CASCADE)
-    datetime = models.DateTimeField() # date & time of the test
+    items = models.ManyToManyField(Item)
+    created_datetime = models.DateTimeField(auto_now=True, null=True, blank=True) # date & time of the test
 
 class Answer(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
@@ -29,11 +26,3 @@ class Answer(models.Model):
 
     answer = models.IntegerField()
     create_datetime = models.DateTimeField()
-
-
-class AnswerSheet(models.Model):
-    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
-
-class ListofAnswerSheet(models.Model):
-    answersheet = models.ForeignKey(AnswerSheet, on_delete=models.CASCADE)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
